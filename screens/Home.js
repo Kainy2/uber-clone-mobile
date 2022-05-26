e90c02e3f4da256a02786dcfbdfe6ed91342defc
Kainy <iartkhay@gmail.com>
2022-05-26 23:52:12 +0100

    stack navigation and google places api

--- screens/Home.js
+++ screens/Home.js
@@ -3,13 +3,28 @@ import React from 'react'
 import tw from 'twrnc'
 import NavOptions from '../components/NavOptions'
 import uber from '../assets/Uber.png'
-
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { GOOGLE_MAPS_APIKEY } from '@env'
 
 const Home = () => {
+
   return (
     <SafeAreaView style={[tw`pt-5`, styles.container]}>
       <View style={tw`p-5`}>
         <Image style={{ width: 100, height: 100, resizeMode: 'contain' }} source={uber} />
+      </View>
+      <View style={{
+        padding: 20,
+        margin: 1,
+        borderWidth: 1,
+        borderColor: 'gray'
+      }} >
+        <GooglePlacesAutocomplete styles={{
+          padding: 20,
+          margin: 1,
+          borderWidth: 1,
+          borderColor: 'gray'
+        }} />
       </View>
       <View style={tw`p-5`}>
         <NavOptions />
