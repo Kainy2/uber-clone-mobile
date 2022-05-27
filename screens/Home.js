6a502d13227b03a79a35f0f3744d582272369620
Kainy <iartkhay@gmail.com>
2022-05-27 23:53:02 +0100

    google maps api integration

--- screens/Home.js
+++ screens/Home.js
@@ -8,25 +8,31 @@ import { GOOGLE_MAPS_APIKEY } from '@env'
 
 const Home = () => {
 
+  const apiKey = 'AIzaSyAwGRGfcv15 - U0arYyUfmacb5PSsnFvrqo'
+
   return (
     <SafeAreaView style={[tw`pt-5`, styles.container]}>
-      <View style={tw`p-5`}>
+      <View style={tw`p-5 pb-0`}>
         <Image style={{ width: 100, height: 100, resizeMode: 'contain' }} source={uber} />
       </View>
-      <View style={{
-        padding: 20,
-        margin: 1,
-        borderWidth: 1,
-        borderColor: 'gray'
-      }} >
-        <GooglePlacesAutocomplete styles={{
-          padding: 20,
-          margin: 1,
-          borderWidth: 1,
-          borderColor: 'gray'
-        }} />
-      </View>
-      <View style={tw`p-5`}>
+      <View style={tw`p-5 pt-0`}>
+
+        <GooglePlacesAutocomplete placeholder='Where From?' debounce={400} query={{ key: apiKey, language: 'en' }}
+          styles={{
+            container: { flex: 0, },
+            textInput: { fontSize: 18, }
+          }}
+          minLength={2}
+          returnKeyType={'search'}
+          fetchDetails={true}
+          enablePoweredByContainer={false}
+          nearbyPlacesAPI={'GooglePlacesSearch'}
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(data, details);
+          }}
+
+        />
         <NavOptions />
       </View>
     </SafeAreaView >
